1c2e578897eb4073e2b099d25217d113a7146796
anastasialukanova <anastasia.lukanova@student.nhlstenden.com>
2024-01-17 19:20:20 +0100

    nav bar very yes

--- src/pages/adminpanel-users/Users.tsx
+++ src/pages/adminpanel-users/Users.tsx
@@ -2,18 +2,34 @@ import React from "react";
 import "./Users.css";
 import NetflixLogo from "../../components/NetflixLogo";
 
+// const users_data = [
+//     {profile_name: "John", email: "test1@hey.com", status: "BLOCKED", birth_date: 01-02-2003, payment: "card"},
+//     {email: "test2@hey.com", payment: "card"},
+//     {email: "test3@hey.com", payment: "card"},
+//     {email: "test4@hey.com", payment: "card"},
+//     {email: "test5@hey.com", payment: "card"},
+//     {email: "test", payment: "card"},
+//     {email: "test", payment: "card"},
+//     {email: "test", payment: "card"}
+// ]
+
 const Users = () => {
     return (
-        <div>
+        <div className="gridBody">
             <div className="navBar">
-            <NetflixLogo />
-            <h2>Admin Panel</h2>
-            <div className="navButtons">
-                <button>Users</button>
-                <button>Media Statistics</button>
-                <button>Docs</button>
+                <NetflixLogo />
+                <h2>Admin Panel</h2>
+                <div className="navButtons">
+                    <button>Users</button>
+                    <button>Media Statistics</button>
+                    <button>Docs</button>
+                </div>
             </div>
-        </div>
+            <div className="header">
+                <header>
+                    <h1>Users</h1>
+                </header>
+            </div>
         </div>
     );
 }
