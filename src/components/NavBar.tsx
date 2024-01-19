216804e9956c4cd2c282d3e164024530ed0083d5
Marijn Veenstra <114413125+MarijnVeenstra@users.noreply.github.com>
2024-01-19 14:13:41 +0100

    fixed navbar styling

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,26 @@
-import NetflixLogo from "./NetflixLogo";
 import React from "react";
+import { Link } from "react-router-dom";
+import NetflixLogo from "./NetflixLogo";
 import "./NavBar.css";
 
-function NavBar (){
-  return(
+function NavBar() {
+  return (
     <div className="navBar">
-        <NetflixLogo />
-        <h2>Admin Panel</h2>
-        <div className="navButtons">
-            <button>Profiles</button>
-            <button>Media Statistics</button>
-            <button>Docs</button>
-        </div>
+      <NetflixLogo />
+      <h2>Admin Panel</h2>
+      <div>
+        <Link to="/profiles">
+          <button  className="navButtons">Profiles</button>
+        </Link>
+        <Link to="/media-statistics">
+          <button  className="navButtons">Media Statistics</button>
+        </Link>
+        <Link to="/docs">
+          <button  className="navButtons">Docs</button>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
+
 export default NavBar;
